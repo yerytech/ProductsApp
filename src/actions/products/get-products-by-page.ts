9ddc863a551e3dcba732d7a2c75b9bf15712966e
yerytech <yerytecnology@gmail.com>
2025-02-11 17:48:10 -0400

    Refactor getProductsByPage function to improve type safety and code readability

--- src/actions/products/get-products-by-page.ts
+++ src/actions/products/get-products-by-page.ts
@@ -1,22 +1,25 @@
 import { tesloApi } from "../../config/api/tesloApi";
-import  { TesloProduct } from "../../infrastructure/interfacea/teslo-products.response";
+import type { Product } from "../../domain/entities/products.entity";
+import { TesloProduct } from "../../infrastructure/interfacea/teslo-products.response";
 import { ProductMapper } from "../../infrastructure/mapper/product.mapper";
 
-export const getProdutsByPage = async (page:number,limit:number =20) => { 
- console.log({page,limit});
- 
+export const getProdutsByPage = async (
+  page: number,
+  limit: number = 20
+): Promise<Product[]> => {
+  console.log({ page, limit });
+
   try {
-   const { data } = await  tesloApi.get<TesloProduct[]>(`/products?offset=${page*10}&limit=${limit}`);
-    const products = data.map(tesloProduct => ProductMapper.tesloProductToEntiTy(tesloProduct))
-   
-   
-     return products;
- } catch (error) {
+    const { data } = await tesloApi.get<TesloProduct[]>(
+      `/products?offset=${page * 10}&limit=${limit}`
+    );
+    const products = data.map((tesloProduct) =>
+      ProductMapper.tesloProductToEntiTy(tesloProduct)
+    );
+
+    return products;
+  } catch (error) {
     console.log(error);
     throw new Error("Error getting products");
-    
-  
- }
-
-
-}
+  }
+};
