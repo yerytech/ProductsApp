a37b98ddba90dc63af563ea207624eeca490f4ee
yerytech <yerytecnology@gmail.com>
2025-02-11 17:41:26 -0400

    Add product fetching functionality and related data structures

---
+++ src/actions/products/get-products-by-page.ts
@@ -0,0 +1,22 @@
+import { tesloApi } from "../../config/api/tesloApi";
+import  { TesloProduct } from "../../infrastructure/interfacea/teslo-products.response";
+import { ProductMapper } from "../../infrastructure/mapper/product.mapper";
+
+export const getProdutsByPage = async (page:number,limit:number =20) => { 
+ console.log({page,limit});
+ 
+  try {
+   const { data } = await  tesloApi.get<TesloProduct[]>(`/products?offset=${page*10}&limit=${limit}`);
+    const products = data.map(tesloProduct => ProductMapper.tesloProductToEntiTy(tesloProduct))
+   
+   
+     return products;
+ } catch (error) {
+    console.log(error);
+    throw new Error("Error getting products");
+    
+  
+ }
+
+
+}
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,9 @@
 import { Button, Icon, Layout, Text } from '@ui-kitten/components';
 import { useAuthStore } from "../../store/auth/useAuthStore";
+import { getProdutsByPage } from "../../../actions/products/get-products-by-page";
 
 export const HomeScreen = () => {
+  getProdutsByPage(1);
   const { logOut } = useAuthStore();
   return (
     <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
